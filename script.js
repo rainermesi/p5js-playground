7b6d4f56a0def09c1a06bdfec70b186d599d911b
Rainer Mesi <rainer.mesi@gmail.com>
2021-05-17 00:49:33 +0300

    added spinning arches

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-var spin = 0.0
-var x = width/2
-var y = height/2
+var spinArr = [];
+var tempArr = [];   
+var stepSize = 50;
+var spin = 0.0;
+
+for (i=-2; i < 2; i += 0.2) {
+    tempArr.push(i)
+};
+for (i=0; i < tempArr.length; i++){
+    spinArr.push([stepSize,tempArr[(Math.floor(Math.random() * tempArr.length))]])
+    stepSize += 50;
+};
+
+function randElem(x) {
+    return x[Math.floor(Math.random * x.length)]
+}
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
   angleMode(DEGREES);
+
 };
 
 function arcSpin(w,h,start,stop,s){
@@ -15,13 +29,25 @@ function arcSpin(w,h,start,stop,s){
     pop();
 }
 
+function populateArcs(x) {
+    for (i=0; i < spinArr.length; i++){
+        arcSpin(x[i][0],x[i][0],0,180,spin*(x[i][1]))
+    }
+}
+
 function draw() {
   background(255);
   noFill();
-  arcSpin(450,450,0,30,spin*2);
-  arcSpin(500,500,0,180,spin);
-  arcSpin(550,550,0,90,spin*(-1));
-  arcSpin(550,550,0,90,spin*(-1));
-  
+  populateArcs(spinArr);
+//  arcSpin(50,50,0,30,spin*2);
+//  arcSpin(350,350,60,130,spin*2.3);
+//  arcSpin(400,400,30,330,spin*(-1.8));
+//  arcSpin(450,450,0,30,spin*2.1);
+//  arcSpin(500,500,0,180,spin);
+//  arcSpin(550,550,0,90,spin*(-1));
+//  arcSpin(550,550,0,90,spin*(-1));
+  console.log(spinArr)
   spin += 0.1
-}
+// noLoop()
+  }
+
